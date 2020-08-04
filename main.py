cf840127f773a51c90ed1ed11a6c9dc64e84d9e1
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-08-05 00:58:13 +0100

    Basic KB read

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def load_knowledge_base():
-    pass
+import sys
 
-def main():
-    kb = load_knowledge_base()
+def load_knowledge_base(kb_path):
+    with open(kb_path, 'r') as f:
+        for line in f:
+            nline = line.strip()[:-1].split('\t')
+            sub, rel, obj = nline
+            
+def main(argv):
+    kb_path = argv[0]
+    kb = load_knowledge_base(kb_path)
     
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
