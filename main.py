088977e582c0fefa100efcc6b64e0541f46e7db1
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-08-08 04:27:53 +0100

    Fixed some metrics bugs

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from rule import Rule
 
 def init_rules(kb, minconf):
     rules = [Rule([atom]) for atom in list(kb.rso.keys())]
-    c = []
-    for rule in rules:
-        sconf = rule.standard_confidence(kb)
-        pconf = rule.pca_confidence(kb)
-        hc = rule.head_coverage(kb)
-        print(rule.rule, rule.support(kb))
 
 def get_rules(kb, minconf=0.1):
     rules = init_rules(kb, minconf)
@@ -17,11 +11,6 @@ def get_rules(kb, minconf=0.1):
 def main(argv):
     kb = KB(argv[0])
 
-    r = Rule(['<wasBornIn>', '<LivesIn>'])
-    r.print_details(kb)
-    #print(r.support(kb))
-    #print(r.head_coverage(kb))
-    #get_rules(kb)
     
 if __name__ == '__main__':
     main(sys.argv[1:])
