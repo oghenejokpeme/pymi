ac7ae05cc1de6b43ceb2a4cf64b1ee98fdfbea89
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-08-08 18:39:48 +0100

    Fixed metrics for binary rules

--- rule.py
+++ rule.py
@@ -1,7 +1,7 @@
-# First naive implementation, refactor later.
+# First naive implementation, currently does not account for variables.
 class Rule:
     def __init__(self, atoms):
-        self.rule = [('x', atom, 'y') for atom in atoms]
+        self.rule = [atom for atom in atoms]
     
     def is_closed(self):
         pass
@@ -13,40 +13,57 @@ class Rule:
         for atom in self.rule[1:]:
             _, rel, _ = atom
             insts = insts.intersection(kb.rels[rel])
-        
+
         return insts
-
-    def support(self, kb):
-        return len(self.get_instantiations(kb))
     
-    def size(self, kb):
-        return len(kb.rels[self.rule[0][1]])
-
-    def head_coverage(self, kb):
-        return self.support(kb)/self.size(kb)
-
-    def standard_confidence(self, kb):
-        # Note: Loops through rule twice. Refactor later.
+    def body_size(self, kb):
+        """Calculates numerator for standard confidence calculation.
+        """
         insts = set()
         for atom in self.rule[1:]:
             _, rel, _ = atom
             insts = insts.union(kb.rels[rel])
         
-        return self.support(kb)/len(insts)
+        return len(insts)
+
+    def pca_body_size(self, kb):
+        """Calculates numerator for PCA standard confidence calculation.
+        """
+        sopairs = set()
+        hsubs = set(kb.rso[self.rule[0][1]].keys())
+        for atom in self.rule[1:]:
+            _, rel, _ = atom
+            asubs = set(kb.rso[rel].keys())
+            isubs = hsubs.intersection(asubs)
+            for sub in isubs:
+                for obj in kb.rso[rel][sub]:
+                    sopairs.add((sub, obj))
+                #print(sub,' -> ', kb.rso[rel][sub])
+        #print(sopairs)
+        return len(sopairs)
+
+    def support(self, kb):
+        return len(self.get_instantiations(kb))
     
+    def head_predicted(self, kb):
+        return len(kb.rels[self.rule[0][1]])
+
+    def head_coverage(self, kb):
+        return self.support(kb)/self.head_predicted(kb)
+
+    def standard_confidence(self, kb):
+        try:
+            return self.support(kb)/self.body_size(kb)
+        except ZeroDivisionError:
+            return float('inf')
+
     # Still incorrect, fix next.
     def pca_confidence(self, kb):
-        insts = self.get_instantiations(kb)
-        
-        neg_count = 0
-        for atom in self.rule:
-            _, rel, _ = atom
-            for sub, obj in insts:
-                neg_objs = kb.rso[rel][sub] - {obj}    
-                neg_count += len(neg_objs)
-        support = self.support(kb)
+        # Rules must have body atoms
+        if len(self.rule) == 1:
+            return float('inf')
 
-        return support/(support+neg_count)
+        return self.support(kb)/self.pca_body_size(kb) 
 
     # For developement
     def print_details(self, kb):
@@ -55,4 +72,6 @@ class Rule:
         print('HC: ', self.head_coverage(kb))
         print('Std Conf: ', self.standard_confidence(kb))
         print('PCA Conf: ', self.pca_confidence(kb))
+        print('Body Size: ', self.body_size(kb))
+        print('PCA Body Size: ', self.pca_body_size(kb))
         print('')
