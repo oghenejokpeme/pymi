3d90aa8dc3c00b6d421d3276c2b891ca822ea55f
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-08-09 22:15:26 +0100

    Some query functions

--- kb.py
+++ kb.py
@@ -1,7 +1,9 @@
 class KB:
     def __init__(self, kb_path):
         # Direct maps.
-        self.rels = {}
+        self.rels = {} #P
+        self.subs = {} #S
+        self.objs = {} #O
 
         # Linked KB
         self.sro = {}
@@ -26,18 +28,20 @@ class KB:
         else:
             subkb[enta] = {entb: {entc}}
     
-    def add_to_relmap(self, sub, rel, obj):
+    def add_to_dmap(self, key, atom, dmap):
         try:
-            self.rels[rel].add((sub, obj))
+            dmap[key].add(atom)
         except KeyError:
-            self.rels[rel] = {(sub, obj)}
+            dmap[key] = {atom}
 
     def process_kb(self, kb_path):
         with open(kb_path, 'r') as f:
             for line in f:
                 atom = line.strip()[:-1].split('\t')
                 sub, rel, obj = atom
-                self.add_to_relmap(sub, rel, obj)
+                self.add_to_dmap(rel, (sub, rel, obj), self.rels)
+                self.add_to_dmap(sub, (sub, rel, obj), self.subs)
+                self.add_to_dmap(obj, (sub, rel, obj), self.objs)
 
                 self.add_to_subkb(sub, rel, obj, self.sro)
                 self.add_to_subkb(sub, obj, rel, self.sor)
@@ -45,3 +49,166 @@ class KB:
                 self.add_to_subkb(rel, obj, sub, self.ros)
                 self.add_to_subkb(obj, sub, rel, self.osr)
                 self.add_to_subkb(obj, rel, sub, self.ors)
+    
+    def instantiate_relation(self, atom):
+        svar, rel, ovar = atom
+        ls = len(svar)
+        os = len(ovar)
+
+        try:
+            if ls == 1 and os == 1:
+                return self.rels[rel]
+            
+            elif ls == 1 and os != 1:
+                insts = {(sub, rel, ovar[1]) 
+                         for sub in self.ros[rel][ovar[1]]}
+                
+                return insts
+
+            elif ls != 1 and os == 1:
+                insts = {(svar[1], rel, obj) 
+                         for obj in self.rso[rel][svar[1]]}
+                
+                return insts
+            
+            elif ls != 1 and os != 1:
+                srels = self.sor[svar[1]][ovar[1]]
+                orels = self.osr[ovar[1]][svar[1]]
+
+                if rel in srels and rel in orels:
+                    return {(svar[1], rel, ovar[1])}
+                else:
+                    return {}
+        
+        except KeyError:
+            return {}
+
+    def get_atom_size(self, atom):
+        svar, rel, ovar = atom
+        ls = len(svar)
+        os = len(ovar)
+        try:
+            if ls == 1 and os == 1:
+                return len(self.rels[rel])
+            
+            elif ls == 1 and os != 1:
+                return len(self.ros[rel][ovar[1]])  
+
+            elif ls != 1 and os == 1:
+                return len(self.rso[rel][svar[1]])
+
+            elif ls != 1 and os != 1:
+                srels = self.sor[svar[1]][ovar[1]]
+                orels = self.osr[ovar[1]][svar[1]]
+
+                if rel in srels and rel in orels:
+                    return 1
+                else:
+                    return 0
+        except KeyError:
+            return 0
+
+    def bind_instantiation(self, atom, inst, rule):
+        asvar, rel, aovar = atom
+        assym = asvar[0]
+        aosym = aovar[0]
+        sinst, _, oinst = inst
+
+        brule = []
+        for batom in rule:
+            bsvar, brel, bovar = batom
+            bssym = bsvar[0]
+            bosym = bovar[0]
+           
+            if assym == bssym and aosym == bosym:
+                brule.append(([bssym, sinst], brel, [bosym, oinst]))
+
+            elif assym == bssym and aosym != bosym:
+                brule.append(([bssym, sinst], brel, [bosym]))
+
+            elif assym != bssym and aosym == bosym:
+                brule.append(([bssym], brel, [bosym, oinst]))
+
+            elif assym == bosym and aosym == bssym:
+                brule.append(([bssym, oinst], brel, [bosym, sinst]))
+
+            elif assym == bosym and aosym != bssym:
+                brule.append(([bssym], brel, [bosym, sinst]))
+
+            elif assym != bosym and aosym == bssym:
+                brule.append(([bssym, oinst], brel, [bosym]))
+
+        return brule
+
+    # Checks for binding for conjuctive query.
+    def exists(self, rule):
+        if len(rule) == 1:
+            return self.get_atom_size(rule[0]) > 0
+        else:
+            s = [self.get_atom_size(atom) for atom in rule]
+            sidx = s.index(min(s))
+            satom = rule[sidx]
+            _, srel, _ = satom
+            qp = [atom for atom in rule if atom != satom]
+            insts = self.instantiate_relation(satom)
+            
+            for inst in insts:
+                qp = self.bind_instantiation(satom, inst, qp)
+                if self.exists(qp):
+                    return True
+        
+        return False
+
+    def rule_instantiation(self, var, atom, sinst, oinst, rule):
+        asvar, rel, aovar = atom
+        assym = asvar[0]
+        aosym = aovar[0]
+        
+        brule = []
+        for batom in rule:
+            bsvar, brel, bovar = batom
+            bssym = bsvar[0]
+            bosym = bovar[0]
+
+            temp_sub = [bssym]
+            temp_obj = [bosym]
+
+            if assym == bssym == var:
+                temp_sub.append(sinst)
+            if aosym == bosym == var:
+                temp_obj.append(oinst)
+            if assym == bosym == var:
+                temp_obj.append(sinst)
+            if aosym == bssym == var:
+                temp_sub.append(oinst)
+
+            brule.append((temp_sub, brel, temp_obj)) 
+
+        return brule        
+
+    def select_distinct(self, var, rule):
+        result = set()
+        
+        s = [self.get_atom_size(atom) for atom in rule]
+        sidx = s.index(min(s))
+        satom = rule[sidx]
+        svar, srel, ovar = satom
+        insts = self.instantiate_relation(satom)
+
+        if var == svar[0] or var == ovar[0]:
+            for inst in insts:
+                sinst, _, oinst = inst
+                qp = self.rule_instantiation(var, satom, sinst, oinst, rule)
+                if self.exists(qp):
+                    if var == svar[0]:
+                        result.add(sinst)
+                    if var == ovar[0]:
+                        result.add(oinst)
+        
+        else:
+            q = [atom for atom in rule if atom != satom]
+            for inst in insts:
+                qp = self.bind_instantiation(satom, inst, q)
+                result.add(select_distinct(var, qp))
+
+        return result
--- rule.py
+++ rule.py
@@ -3,70 +3,10 @@ class Rule:
     def __init__(self, atoms):
         self.rule = [atom for atom in atoms]
     
-    def is_closed(self):
-        pass
-
-    def get_instantiations(self, kb):
-        """The intersection of all instantiations for each atom in a
-        rule."""
-        insts = kb.rels[self.rule[0][1]]
-        for atom in self.rule[1:]:
-            _, rel, _ = atom
-            insts = insts.intersection(kb.rels[rel])
-
-        return insts
-    
-    def body_size(self, kb):
-        """Calculates numerator for standard confidence calculation.
-        """
-        insts = set()
-        for atom in self.rule[1:]:
-            _, rel, _ = atom
-            insts = insts.union(kb.rels[rel])
-        
-        return len(insts)
-
-    def pca_body_size(self, kb):
-        """Calculates numerator for PCA standard confidence calculation.
-        """
-        sopairs = set()
-        hsubs = set(kb.rso[self.rule[0][1]].keys())
-        for atom in self.rule[1:]:
-            _, rel, _ = atom
-            asubs = set(kb.rso[rel].keys())
-            isubs = hsubs.intersection(asubs)
-            for sub in isubs:
-                for obj in kb.rso[rel][sub]:
-                    sopairs.add((sub, obj))
-                #print(sub,' -> ', kb.rso[rel][sub])
-
-        return len(sopairs)
-
-    def support(self, kb):
-        return len(self.get_instantiations(kb))
-    
-    def head_predicted(self, kb):
-        return len(kb.rels[self.rule[0][1]])
-
-    def head_coverage(self, kb):
-        return self.support(kb)/self.head_predicted(kb)
-
-    def standard_confidence(self, kb):
-        try:
-            return self.support(kb)/self.body_size(kb)
-        except ZeroDivisionError:
-            return float('inf')
-
-    def pca_confidence(self, kb):
-        # For empty rules, as rules must have body atoms.
-        if len(self.rule) == 1:
-            return float('inf')
-
-        return self.support(kb)/self.pca_body_size(kb) 
-
     # For developement
     def print_details(self, kb):
         print(self.rule)
+        '''
         print('Support: ', self.support(kb))
         print('HC: ', self.head_coverage(kb))
         print('Std Conf: ', self.standard_confidence(kb))
@@ -74,3 +14,4 @@ class Rule:
         print('Body Size: ', self.body_size(kb))
         print('PCA Body Size: ', self.pca_body_size(kb))
         print('')
+        #'''
