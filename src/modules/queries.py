a8aff19debcfbd05f0e5565057d6c2aa57013f50
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-08-16 19:58:11 +0100

    Further refactor

--- src/modules/queries.py
+++ src/modules/queries.py
@@ -1,4 +1,4 @@
-from .rule import Atom
+from modules import rule
 
 def get_atom_instantiations(atom, db):
     try:
@@ -46,13 +46,13 @@ def instantiate_query_with_atom_bindings(query, iatom):
         if iatom.objvar == qatom.subvar:
             si = iatom.objinst
 
-        nquery.add(Atom(sv, si, rel, ov, oi))
+        nquery.add(rule.Atom(sv, si, rel, ov, oi))
     
     return nquery
 
 def check_query_existence(query, db):
-    """The query is the list of all body atoms of a rule. That is,
-    it is the rule with the head atom."""
+    """The query is the set of all body atoms of a rule. That is, it
+    is the rule with or without the head atom."""
 
     if len(query) == 1:
         return get_atom_size(query.pop(), db) > 0
@@ -62,55 +62,39 @@ def check_query_existence(query, db):
         query.remove(satom)
         insts = get_atom_instantiations(satom, db)
 
-        for inst_atom in insts:
-            qp = instantiate_query_with_atom_bindings(query, inst_atom)
+        for iatom in insts:
+            qp = instantiate_query_with_atom_bindings(query, iatom)
             if check_query_existence(qp, db):
                 return True
     
     return False
 
 def instantiate_query_with_var_bindings(var, query, iatom):
-    isub, _, iobj = iatom
-    isubvar = isub[0]
-    iobjvar = iobj[0]
-
-    bquery = set()
-    for qatom in query:
-        qsub, qrel, qobj = qatom
-        qsubvar = qsub[0]
-        qobjvar = qobj[0]
-        temp_sub = qsub
-        temp_obj = qobj
-
-        if isubvar == qsubvar == var:
-            temp_sub = isub
-        elif iobjvar == qobjvar == var:
-            temp_obj = iobj
-        elif isubvar == qobjvar == var:
-            temp_obj = isub
-        elif iobjvar == qsubvar == var:
-            temp_sub = iobj
-        
-        bquery.add((temp_sub, qrel, temp_obj))
+    """Instantiates the object/object of the atoms in a query given
+    an input atom (iatom) and the variabe one would like instantiate
+    or change its current instantiation."""
     
-    if isubvar == var:
-        return (isub[1], bquery)
-    elif iobjvar == var:
-        return (iobj[1], bquery)
+    nquery = set()
+    for qatom in query:
+        sv, si, rel, ov, oi = qatom
+
+        if iatom.subvar == sv == var:
+            si = iatom.subinst
+        elif iatom.objvar == ov == var:
+            oi = iatom.objinst
+        elif iatom.subvar == ov == var:
+            oi = iatom.subinst
+        elif iatom.objvar == sv == var:
+            si = iatom.objinst
+        
+        nquery.add(rule.Atom(sv, si, rel, ov, oi))
+
+    if iatom.subvar == var:
+        return (iatom.subinst, nquery)
+    elif iatom.objvar == var:
+        return (iatom.objinst, nquery)
     else:
         raise Exception('Variable binding error!')
-
-def var_in_atom(var, atom):
-    """Checks if variable is in an atom and the variable has not been
-    assigned an entity."""
-    sub, _, obj = atom
-
-    if var in sub and len(sub) == 1:
-        return True
-    elif var in obj and len(obj) == 1:
-        return True
-    else:
-        return False
 
 def select_distinct_for_query(var, query, db, result):
     """Identifies all entities for ?var in a query which satisfies 
@@ -122,100 +106,65 @@ def select_distinct_for_query(var, query, db, result):
     satom = s[min(s)]
     insts = get_atom_instantiations(satom, db)
 
-    if var_in_atom(var, satom):
-        for inst_atom in insts:
-            ent, qp = instantiate_query_with_var_bindings(var, query, inst_atom)
+    if satom.var_in_atom(var):
+        for iatom in insts:
+            ent, qp = instantiate_query_with_var_bindings(var, query, iatom)
             
             if check_query_existence(qp, db):
                 result.add(ent)
     
     else:
         query.remove(satom)
-        for inst_atom in insts:
-            qp = instantiate_query_with_atom_bindings(query, inst_atom)
+        for iatom in insts:
+            qp = instantiate_query_with_atom_bindings(query, iatom)
             result.union(select_distinct_for_query(var, qp, db, result))
 
     return result
 
-def is_expensive(head, body):
-    """A rule is expensive if:
-    1. It contains variables other than those that appear in the head
-       atom.
-    2. If these additional variables define a single path between head
-       variables."""
-    # @TODO: Move to rule file. This is a rule only computation.
-    sub, _, obj = head
-    checks = {True:0, False:0}
-    for batom in body:
-        bsub, _, bobj = batom
-        if sub[0] == bsub[0] and obj[0] == bobj[0]:
-            checks[False] += 1
-        elif sub[0] == bobj[0] and obj[0] == bsub[0]:
-            checks[False] += 1
-        else:
-            checks[True] += 1
-
-    if checks[False] == len(body):
-        return False
-    else:
-        # Still needs the code that checks for condition 2 above.
-        return True
-
 def get_count(head, body, db):
-    """The query may or may not include the head of a rule.
+    """The query may or may not include the head of a rule. Returns:
     Support -> Head and body.
     CWA numerator -> Just body. Head = None."""
 
     q = body
     if head:
-        if not is_expensive(head, body):
+        if not rule.is_expensive(head, body):
             q = body.union({head}) 
         else:
-            # Perform approximate metric computation.
+            #@TODO: Write code that performs approximate metric computation.
             return False
     else:
         import random
         head = random.sample(body, 1)[0]
-        
-    sub, rel, obj = head
-    subvar = sub[0]
-    objvar = obj[0]
 
     count = 0
-    sub_distinct = select_distinct_for_query(subvar, q, db, set())
+    sub_distinct = select_distinct_for_query(head.subvar, q, db, set())
     for subinst in sub_distinct:
-        tiatom = ((subvar, subinst), rel, obj) 
-        _, qp = instantiate_query_with_var_bindings(subvar, q, tiatom)
-        obj_distinct = select_distinct_for_query(objvar, qp, db, set()) 
+        iatom = head.make_subject_instance(subinst)
+        _, qp = instantiate_query_with_var_bindings(head.subvar, q, iatom)
+        obj_distinct = select_distinct_for_query(head.objvar, qp, db, set())
         count += len(obj_distinct)
-    
+
     return count
 
+
 def get_pca_count(fvar, head, body, db):
-    hsub, _, hobj = head
-    hsubvar = hsub[0]
-    hobjvar = hobj[0]
-    
+    """Returns PCA numerator."""
+
     ypvar = ''
-    if fvar == hsubvar:
-        ypvar = hobjvar
-    elif fvar == hobjvar:
-        ypvar = hsubvar
+    if fvar == head.subvar: ypvar = head.objvar
+    elif fvar == head.objvar: ypvar = head.subvar
 
-    insts = get_atom_instantiations(head, db)
     dn = set()
-    for ainst in insts:
-        asub, _, aobj = ainst
-        asvar, asent = asub
-        aovar, aoent = aobj 
-
-        _, qp = instantiate_query_with_var_bindings(fvar, body, ainst)
+    insts = get_atom_instantiations(head, db)
+    for iatom in insts:
+        _, qp = instantiate_query_with_var_bindings(fvar, body, iatom)
         obj_distinct = select_distinct_for_query(ypvar, qp, db, set())
         
         for dobj in obj_distinct:
-            if fvar == asvar:
-                dn.add((asent, dobj))
-            elif fvar == aovar:
-                dn.add((aoent, dobj))
+            if fvar == iatom.subvar:
+                dn.add((iatom.subinst, dobj))
+            elif fvar == iatom.objvar:
+                dn.add((iatom.objinst, dobj))
     
     return len(dn)
--- src/modules/rule.py
+++ src/modules/rule.py
@@ -48,3 +48,31 @@ class Atom:
             out = ('Attempting subject instantiation of already instantiated' +
                    'atom variable.')
             raise Exception(out)
+
+    def var_in_atom(self, var):
+        if self.subvar == var or self.objvar == var:
+            return True
+        else:
+            return False
+
+# @TODO: Writes code that handles condition 2.
+def is_expensive(head, body):
+    """A rule is expensive if:
+    1. It contains variables other than those that appear in the head
+       atom.
+    2. If these additional variables define a single path between head
+       variables."""
+
+    checks = {True:0, False:0}
+    for batom in body:
+        if head.subvar == batom.subvar and head.objvar == batom.objvar:
+            checks[False] += 1
+        elif head.subvar == batom.objvar and head.objvar == batom.subvar:
+            checks[False] += 1
+        else:
+            checks[True] += 1
+
+    if checks[False] == len(body):
+        return False
+    else:
+        return True
