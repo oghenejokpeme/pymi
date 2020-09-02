f7be0203b55cda7aea09c6b32865ffbb9a4fbb3a
oghenejokpeme <oghenejokpeme.orhobor@gmail.com>
2020-09-02 21:05:14 +0100

    Core

---
+++ src/modules/core.py
@@ -0,0 +1,11 @@
+def add_dangling_atom(head, body):
+    pass
+
+def add_closing_atom(head, body):
+    pass
+
+def add_instantiated_atom(head, body):
+    pass
+
+def learn_rules(init_atoms):
+    pass
